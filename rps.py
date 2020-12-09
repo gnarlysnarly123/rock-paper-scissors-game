41e2a55564fa08dba15c8ef194a603918a564803
gnarlysnarly123 <74368984+gnarlysnarly123@users.noreply.github.com>
2020-12-09 19:35:47 +0800

    Update rps.py

--- rps.py
+++ rps.py
@@ -28,25 +28,25 @@ while True:
     #condition for winning
     if comp==1:
         if choice == 2:
-            print("You win!")
+            print("=====You win!=====")
         elif choice == 1:
-            print("Tie!")
+            print("=====Tie!=====")
         else:
-            print("You lose!")
+            print("=====You lose!=====")
     elif comp==2:
         if choice == 1:
-            print("You lose!")
+            print("=====You lose!=====")
         elif choice == 2:
-            print("Tie!")
+            print("=====Tie!=====")
         else:
-            print("You win!")
+            print("=====You win!=====")
     else:
         if choice == 1:
-            print("You win!")
+            print("=====You win!=====")
         elif choice == 2:
-            print("You lose!")
+            print("=====You lose!=====")
         else:
-            print("Tie!")
+            print("=====Tie!=====")
     print("Do you want to play again? (y/n)")
     ans = input()
     #exit loop
